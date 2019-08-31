e0c2e1c34d70b98434d489c0635267e9283adf66
RoB8080 <75566977@qq.com>
2019-08-31 19:49:40 +0800

    add .gitignore

--- prompts.js
+++ prompts.js
@@ -1,22 +1,20 @@
 module.exports = [
-    module.exports = [
-        {
-            name: "vuex",
-            type: "confirm",
-            message: `是否要使用 vuex?`,
-            default: true
-        },
-        {
-            name: "i18n",
-            type: "confirm",
-            message: `是否要使用 i18n (国际化)?`,
-            default: false
-        },
-        {
-            name: "element",
-            type: "confirm",
-            message: `是否要使用 element-ui?`,
-            default: false
-        },
-    ]
+    {
+        name: "vuex",
+        type: "confirm",
+        message: `是否要使用 vuex?`,
+        default: true
+    },
+    {
+        name: "i18n",
+        type: "confirm",
+        message: `是否要使用 i18n (国际化)?`,
+        default: false
+    },
+    {
+        name: "element",
+        type: "confirm",
+        message: `是否要使用 element-ui?`,
+        default: false
+    },
 ];
